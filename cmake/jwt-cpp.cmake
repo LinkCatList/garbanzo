c8dd135d71717650a5d4b0faac8c0ed68481d8f6
HARSIS <sewa5haritonov@yandex.ru>
2024-05-09 22:35:00 +0300

    сёма сосёт

--- cmake/jwt-cpp.cmake
+++ cmake/jwt-cpp.cmake
@@ -1,10 +1,9 @@
 include(FetchContent)
-
-FetchContent_Declare(
-    jwt-cpp
+fetchcontent_declare(jwt-cpp 
     GIT_REPOSITORY https://github.com/Thalhammer/jwt-cpp.git
+    GIT_TAG 08bcf77a687fb06e34138e9e9fa12a4ecbe12332 # v0.7.0 release
 )
-
-FetchContent_MakeAvailable(jwt-cpp)
-
-target_link_libraries(${PROJECT_NAME} PRIVATE jwt-cpp)
+set(JWT_BUILD_EXAMPLES OFF CACHE BOOL "disable building examples" FORCE)
+fetchcontent_makeavailable(jwt-cpp)
+ 
+target_link_libraries(${PROJECT_NAME} PRIVATE jwt-cpp::jwt-cpp)
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,18 @@
+#include <iostream>
 #include <httplib.h>
 #include "Database.h"
 #include "Handles.h"
 #include "../include/laserpants/dotenv/dotenv.h"
+#include <jwt-cpp/jwt.h>
 
 int32_t main() {
+
+    std::string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXUyJ9.eyJpc3MiOiJhdXRoMCIsInNhbXBsZSI6InRlc3QifQ.lQm3N2bVlqt2-1L-FsOjtR6uE-L4E9zJutMWKIe1v1M";
+    auto decoded = jwt::decode(token);
+ 
+    for(auto& e : decoded.get_payload_json())
+        std::cout << e.first << " = " << e.second << std::endl;
+    
     dotenv::init();
     const std::string URL = std::getenv("URL");
 
