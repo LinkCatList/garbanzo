4a1c52e3a4be30b6f0c20128e2abfc6af3047e2a
LinkCatList <vigalok@bk.ru>
2024-05-09 12:03:09 +0300

    aboba

--- src/Validation.h
+++ src/Validation.h
@@ -1,5 +1,6 @@
 #include <cctype>
 #include <string>
+#include "Base.h"
 
 
 inline bool validate_password (std::string &password) {
@@ -33,4 +34,9 @@ inline bool validate_login (std::string &login) {
 
 inline bool validate_city (std::string &city) {
 
+}
+
+inline bool validate_user (User &u) {
+    return validate_password(u.password) && validate_city(u.city) &&
+        validate_email(u.email) && validate_login(u.login);
 }
